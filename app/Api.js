a226cadbf37787ecd87f4d41cff0b6433864daac
AshDevInd <94217099+AshDevInd@users.noreply.github.com>
2025-07-24 08:26:55 +0000

    api work

---
+++ app/Api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: 'https://api.example.com', // Your base URL here
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const fetchData = async (endpoint, data = null) => {
+  try {
+    const response = await axiosInstance.post(endpoint, data);
+    return response.data;
+  } catch (error) {
+    throw new Error(error.message || 'Error fetching data');
+  }
+};
+
+export const createPost = (newPost) => fetchData('/posts', newPost);
